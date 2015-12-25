a8467e098ca27d11138143a56914d09011a537e6
strongQ <1270187870@qq.com>
2015-12-25 18:19:22 +0800

    Atom now 2015 12 15

--- Workspace/test.js
+++ Workspace/test.js
@@ -20,3 +20,12 @@ fs.readFile('sample.txt','utf-8',function(err,data){
 	}
 
 });
+//读取二进制文件
+fs.readFile('one.jpg',function(err,data){
+	if (err){
+		console.log(err);
+	}else{
+		console.log(data);
+		console.log(data.length+'bytes');
+	}
+})
