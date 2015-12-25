4414a0fec8ddb64d676414881784620daa8a0e23
strongQ <1270187870@qq.com>
2015-12-25 13:02:28 +0800

    node perfect

--- Workspace/test.js
+++ Workspace/test.js
@@ -10,3 +10,13 @@ if (typeof(window)=='undefined'){
 }else{
 	console.log("browser");
 }
+
+var fs=require('fs');
+fs.readFile('sample.txt','utf-8',function(err,data){
+	if (err){
+		console.log(err);
+	}else{
+		console.log(data);
+	}
+
+});
